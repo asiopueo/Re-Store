fab312987c204a4561bf570437a68eaf56faad11
Martin Lippl <martin.lippl@ccl-it.li>
2025-10-02 15:16:30 +0200

    Ende Abschnitt 2 -- API Basics

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,22 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 app.MapControllers();
+
+DbInitializer.InitDb(app);
 
 app.Run();
