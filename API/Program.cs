eb0d4631035b58baa2ec4081ac3fbb52ea73eb6e
Martin Lippl <martin.lippl@ccl-it.li>
2025-10-02 13:01:00 +0200

    Start Abschnitt 2 -- API Basics

---
+++ API/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
