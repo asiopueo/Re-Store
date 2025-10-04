e880c7cd0a4081333c397d87970948345b59614a
Martin Lippl <martin.lippl@ccl-it.li>
2025-10-04 13:25:36 +0200

    Start Abschnitt 4 - Routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
-import type { Product } from "../models/product";
-import Catalog from "../../features/catalog/Catalog";
+import { useState } from "react";
 import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
 
 
 function App() {
-    const [products, setProducts] = useState<Product[]>([]);
+
     const [darkMode, setDarkMode] = useState<boolean>(true);
     const palleteType = darkMode ? 'dark' : 'light';
     const theme = createTheme({
@@ -18,12 +17,6 @@ function App() {
             }
         }
     })
-
-    useEffect(() => {
-        fetch('https://localhost:5001/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data))
-    }, [])
 
     return (
         <ThemeProvider theme={theme}>
@@ -37,7 +30,7 @@ function App() {
                     py: 6
                 }}>
                     <Container maxWidth='xl' sx={{mt: 8}}>
-                        <Catalog products={products} />
+                        <Outlet />
                     </Container>
                 </Box>
             </CssBaseline>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,11 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import App from './app/layout/App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/routes/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
